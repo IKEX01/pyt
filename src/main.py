3479d2ec65c0fadb2b026cb88330c35917e4172e
Ponlponl123 <my.devpattarapong@gmail.com>
2025-02-16 13:20:16 +0700

    missing apis route

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from src.routers import search, browsing, explore, watch, library
+from src.routers import search, browsing, explore, watch, library, playlists, podcasts, uploads
 
 app = FastAPI()
 app.include_router(search.router)
@@ -7,6 +7,9 @@ app.include_router(browsing.router)
 app.include_router(explore.router)
 app.include_router(watch.router)
 app.include_router(library.router)
+app.include_router(playlists.router)
+app.include_router(podcasts.router)
+app.include_router(uploads.router)
 
 @app.get("/")
 def root():
